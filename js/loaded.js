f78873f68a5ac1a0ef6d5a6edede77816d85f3ce
PY <70672659+PYastreboff@users.noreply.github.com>
2024-06-14 17:02:28 +1000

    updated portfolio

--- js/loaded.js
+++
@@ -1,7 +0,0 @@
-function loaded() {
-    //document.getElementById("loader").style.display = "none"; // remove lading screen
-    //document.getElementById("content").style.display = "initial"; // show main website content
-    console.log("done>");
-}
-//document.getElementById("loader").style.display = "none"; // remove lading screen
-//document.body.style.display = "initial"; // show main website content
